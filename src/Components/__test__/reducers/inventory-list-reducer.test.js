0e095a87cdddecbacb69a467a96c4494917e2331
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-12-15 14:51:37 -0800

    reducer file, reducer test file, first test

---
+++ src/Components/__test__/reducers/inventory-list-reducer.test.js
@@ -0,0 +1,7 @@
+import inventoryListReducer from '../reducers/inventory-list-reducer';
+
+describe('inventoryListReducer', () => {
+    test('Should return default state if there is no action type passed into the reduce', () => {
+        expect(inventoryListReducer({}, {type: null})).toEqual({});
+    });
+});
