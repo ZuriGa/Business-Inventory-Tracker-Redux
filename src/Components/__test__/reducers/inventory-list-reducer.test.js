0e8d2afad70ba351389cddf1bf86c474fd8f4965
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-12-15 15:03:03 -0800

    first test, good fail

--- src/Components/__test__/reducers/inventory-list-reducer.test.js
+++ src/Components/__test__/reducers/inventory-list-reducer.test.js
@@ -1,4 +1,4 @@
-import inventoryListReducer from '../reducers/inventory-list-reducer';
+import inventoryListReducer from '../../../reducers/inventory-list-reducer';
 
 describe('inventoryListReducer', () => {
     test('Should return default state if there is no action type passed into the reduce', () => {
