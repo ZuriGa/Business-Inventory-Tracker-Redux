ea91efe19f700b540ba97e55e741b361a9e396b3
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-12-15 16:35:00 -0800

    second test passed, inventory list reducer

--- src/reducers/inventory-list-reducer.js
+++ src/reducers/inventory-list-reducer.js
@@ -1,5 +1,22 @@
 const reducer =(state = {}, action) => {
-    return state
+    const { name, origin, price, roast, size, flavor, poundsLeft, id } = action;
+    switch (action.type) {
+        case 'ADD_INVENTORY':
+            return Object.assign({}, state, {
+                [id]: {
+                    name: name, 
+                    origin: origin,
+                    price: price,
+                    roast: roast,
+                    size: size,
+                    flavor: flavor,
+                    poundsLeft: poundsLeft,
+                    id: id
+                }
+            });
+        default:
+            return state;
+    }
 };
 
 export default reducer;
