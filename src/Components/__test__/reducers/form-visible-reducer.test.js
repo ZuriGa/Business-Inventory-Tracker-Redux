3489ec2c0e5a0a9517d1720ff1bc5db1cd7ae1bd
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-12-16 15:49:25 -0800

    second test, fail

--- src/Components/__test__/reducers/form-visible-reducer.test.js
+++ src/Components/__test__/reducers/form-visible-reducer.test.js
@@ -4,4 +4,8 @@ describe('formVisibleReducer', () => {
     test('Should return default state if not action type is recognized', () => {
         expect(formVisibleReducer(false, { type: null })).toEqual(false);
     });
+
+    test('Should toggle form visibility state to true', () => {
+        expect(formVisibleReducer(false, { type: 'TOGGLE_FORM'})).toEqual(true);
+    });
 });
