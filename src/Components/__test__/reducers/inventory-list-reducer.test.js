0979377b9559da9454811b974faedf97230d8a08
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-12-15 17:47:26 -0800

    third test, good fail

--- src/Components/__test__/reducers/inventory-list-reducer.test.js
+++ src/Components/__test__/reducers/inventory-list-reducer.test.js
@@ -3,7 +3,8 @@ import inventoryListReducer from '../../../reducers/inventory-list-reducer';
 describe('inventoryListReducer', () => {
 
     let action;
-    const coffeeInventory = {
+    const currentState = {
+        1: {
         name: 'Light City',
         origin: 'Costa Rica',
         price: '$17.00',
@@ -12,7 +13,17 @@ describe('inventoryListReducer', () => {
         flavor: 'Our Costa Rica coffee is a delicate showing with bright notes of bergamot and lemongrass.',
         poundsLeft: 130,
         id: '1'
-    };
+    }, 2:     {
+        name: 'American Roast',
+        origin: 'Guatemala',
+        price: '$19.00',
+        roast: 'Medium roast',
+        size: '1 lb',
+        flavor: 'Our Organic House blend balances mild acidity, medium body, and walnut flavor notes in perfect harmony.',
+        poundsLeft: 130,
+        id: '2'
+    }
+}
 
 
     test('Should return default state if there is no action type passed into the reducer', () => {
@@ -20,7 +31,7 @@ describe('inventoryListReducer', () => {
     });
 
     test('Should successfully add new coffee inventory to mainCoffeeList', () => {
-        const { name, origin, price, roast, size, flavor, poundsLeft, id } = coffeeInventory;
+        const { name, origin, price, roast, size, flavor, poundsLeft, id } = currentState;
         action = {
             type: 'ADD_INVENTORY',
             name: name, 
@@ -42,6 +53,25 @@ describe('inventoryListReducer', () => {
                 flavor: flavor,
                 poundsLeft: poundsLeft,
                 id: id
+            }
+        });
+    });
+
+    test('Should successfully delete an inventory item', () => {
+        action = {
+            type: 'DELETE_INVENTORY',
+            id: 1
+        };
+        expect(inventoryListReducer(currentState, action)).toEqual({
+            2: {
+                name: 'American Roast',
+                origin: 'Guatemala',
+                price: '$19.00',
+                roast: 'Medium roast',
+                size: '1 lb',
+                flavor: 'Our Organic House blend balances mild acidity, medium body, and walnut flavor notes in perfect harmony.',
+                poundsLeft: 130,
+                id: '2'
             }
         });
     });
