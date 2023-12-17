532019accadab8f4520fd0f2d10bec4adeb30b06
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-12-16 17:34:19 -0800

    test passed

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,18 @@ export const deleteInventory = id => ({
 export const toggleForm = () => ({
     type: 'TOGGLE_FORM'
 });
+
+export const addInventory = (coffee) => {
+    const { name, origin, price, roast, size, flavor, poundsLeft, id } = coffee;
+    return {
+        type: 'ADD_INVENTORY',
+        name: name,
+        origin: origin,
+        price: price,
+        roast: roast,
+        size: size,
+        flavor: flavor,
+        poundsLeft: poundsLeft,
+        id: id
+    } 
+}
