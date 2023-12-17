723d0bbdd6069a377de0a4dd420462a28f58380a
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-12-16 16:10:41 -0800

    first test passed, root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,8 @@ import inventoryListReducer from './inventory-list-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-    // formVisibleOnPage: formVisibleReducer,
-    // mainCoffeeList: inventoryListReducer
+    formVisibleOnPage: formVisibleReducer,
+    mainCoffeeList: inventoryListReducer
 });
 
 export default rootReducer;
